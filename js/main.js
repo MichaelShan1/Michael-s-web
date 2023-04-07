c1e82d8030ef9765841b49e08ac8481caa9a2129
Michael Shan <1016658371@qq.com>
2023-04-07 17:45:32 +0800

    Add lazy loading for images

--- js/main.js
+++ js/main.js
@@ -11,3 +11,21 @@ for (let i = 0; i < btnScroll.length; i++) {
     QRcode.scrollIntoView({ behavior: 'smooth' });
   });
 }
+
+//lazy loading image
+const imgLazy = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, obeserver) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  obeserver.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+imgLazy.forEach((img) => imgObserver.observe(img));
